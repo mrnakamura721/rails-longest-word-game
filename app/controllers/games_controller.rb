cf270f4186fad533b25908c0c51f725033a31d6f
Momotaro Nakamura <mrnakamura721@gmail.com>
2020-05-11 17:03:28 +0900

    Completed Longest Word Game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,21 +7,22 @@ class GamesController < ApplicationController
   end
 
   def score
-    letters_array = params[:letters].split(' ')
+    @letters_array = params[:letters].split(' ')
+    letters_list = @letters_array.join(', ')
+
     url = "https://wagon-dictionary.herokuapp.com/#{params[:answer]}"
     answer_array = params[:answer].split('')
-
     word_hash = open(url).read
     word_parse = JSON.parse(word_hash)
     word_check = word_parse['found']
 
     @play_game =
-      if word_check == true && (answer_array - letters_array).empty?
+      if word_check == true && (answer_array - @letters_array).empty?
         "Congratulations! '#{params[:answer].capitalize}' is a valid English word!"
-      elsif word_check == false && (answer_array - letters_array).empty?
+      elsif word_check == false && (answer_array - @letters_array).empty?
         "Sorry but '#{params[:answer]}' does not seem to be a valid Enlgish word."
-      elsif (answer_array - letters_array).empty? == false
-        "Sorry but '#{params[:answer]}' can't be built out of #{letters_array}"
+      elsif (answer_array - @letters_array).empty? == false
+        "Sorry but '#{params[:answer]}' can't be built out of the above letters"
       end
   end
 end
